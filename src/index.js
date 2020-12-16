4df21417b497e808314b6dcc0432355fdc5a1697
Gabriel <luisgabrielarevalo@hotmail.com>
2020-12-16 16:47:45 -0500

    Index modificado

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import './styles.css';
 import { Todo, TodoList } from './classes'                             // Busca por defecto el archivo index.js
 import { crearTodoHtml } from './js/componentes';
 
+const footer          = document.querySelector('.footer');
 
 export const todoList = new TodoList();
 
 // todoList.todos.forEach(todo =>( crearTodoHtml( todo ) ));    // For each para uno más argumentos
 todoList.todos.forEach( crearTodoHtml );                        // For each para un solo argumento
 
-
+if(todoList.todos.length > 0){ footer.classList.remove('hidden') };
 // const newTodo = new Todo('Aprender Javascript');
 // todoList.nuevoTodo( newTodo );
 
